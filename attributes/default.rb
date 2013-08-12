de8feaf9798b31c8ad47f029d9d0cefd5513f610
Jesse Nelson <spheromak@gmail.com>
2013-08-12 11:43:42 -0700

    remove binary location atttribute
    This isn't being used for now.

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,11 +7,13 @@ default[:etcd][:install_method] = "binary"
 # more args ?
 default[:etcd][:extra_args] = ""
 
-# url to the release, and sha256 sum
-default[:etcd][:url] = "https://github.com/coreos/etcd/releases/download/v0.1.0/etcd-v0.1.0-Linux.tar.gz"
+# Release to install
+default[:etcd][:version] = "0.1.0"
+# Sha for github tarball
 default[:etcd][:sha256] = "0089111a4c36e8d481efb91e847dbf498c4db2e33c43a1103fa8fb77f904dcb0"
+# Use this to supply your own url to a tarball
+default[:etcd][:url] = nil
 
 default[:etcd][:port] = 4001
 default[:etcd][:raft_port] = 7001
-default[:etcd][:bin] = "/usr/local/bin/etcd"
 default[:etcd][:state_dir] = "/var/cache/etcd/state"
