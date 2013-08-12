f2207540948922b9cf39c6534cc6404060447c97
Jesse Nelson <spheromak@gmail.com>
2013-08-12 11:44:58 -0700

    binary install asseembles git url from version attribute

--- recipes/binary_install.rb
+++ recipes/binary_install.rb
@@ -2,9 +2,21 @@
 # Installs etcd from released tarballs
 #
 
+
+# assemble default url or use supplied url in attributes
+# we can assemble the url from the version
+#
+version = node[:etcd][:version]
+package = "etcd-v#{version}-#{node[:os].capitalize}.tar.gz"
+package_url = "https://github.com/coreos/etcd/releases/download/v#{version}/#{package}"
+if node[:etcd][:url]
+  package_url = node[:etcd][:url]
+end
+
 ark "etcd" do
   has_binaries [ "etcd", "etcdctl" ]
-  url node[:etcd][:url]
+  version node[:etcd][:version]
+  url package_url
   checksum node[:etcd][:sha256]
   action :install
 end
