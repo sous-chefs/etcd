8274e0297cb7b2de55acd6bcc69b6b9cf711c6ee
Jesse Nelson <spheromak@gmail.com>
2013-08-12 18:41:36 -0700

    simplify the startup args so that they can be used by a wraper cook

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,11 +1,8 @@
 #
-# If you are seting up a clustere of etcd's
-# set your seed node (first cluster node here)
-default[:etcd][:seed_node] = nil
 default[:etcd][:install_method] = "binary"
 
-# more args ?
-default[:etcd][:extra_args] = ""
+# service start args to pass
+default[:etcd][:args] = ""
 
 # Release to install
 default[:etcd][:version] = "0.1.0"
@@ -14,6 +11,4 @@ default[:etcd][:sha256] = "0089111a4c36e8d481efb91e847dbf498c4db2e33c43a1103fa8f
 # Use this to supply your own url to a tarball
 default[:etcd][:url] = nil
 
-default[:etcd][:port] = "0.0.0.0:4001"
-default[:etcd][:raft_port] = "0.0.0.0:7001"
 default[:etcd][:state_dir] = "/var/cache/etcd/state"
--- recipes/_service.rb
+++ recipes/_service.rb
@@ -5,18 +5,8 @@
 
 directory File.dirname node[:etcd][:state_dir]
 
-args =  " -c #{node[:etcd][:port]}"
-args << " -s #{node[:etcd][:raft_port]}"
-args << " -n #{node[:fqdn]}"
-args << " -d #{node[:etcd][:state_dir]}"
-args << " #{node[:etcd][:extra_args]}"
-if node[:etcd][:seed_node]
-  args << " -C #{node[:etcd][:seed_node]}"
-end
-
 template "/etc/init/etcd.conf" do
   mode 0644
-  variables(args: args)
 end
 
 service "etcd" do
