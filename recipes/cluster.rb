404c58b0d93a3589487fb42bd280a81eb1874c81
Jesse Nelson <spheromak@gmail.com>
2014-02-13 11:01:22 -0800

    multiple rubocop fixes.
    rework the logic so that the recipe isn't wrapped in a giant if block.
    cleanup some of the other issues.

--- recipes/cluster.rb
+++ recipes/cluster.rb
@@ -11,75 +11,76 @@ require 'resolv'
 Etcd.node = node
 
 # make sure we validate
-if node[:etcd][:discovery].length > 0 then
-  log 'Use discovery URL to gather the peers'
+if node[:etcd][:discovery].length > 0
+  include_recipe 'etcd'
+  return
+end
+
+msg = '`node[:etcd][:seed_node]` is required to bootstrap a cluster'
+log msg do
+  level :error
+  not_if { node[:etcd][:seed_node] }
+end
+
+# Hostnames and/or ip addresses of current node
+my_ip = Resolv.getaddress(node[:fqdn]) || Resolv.getaddress(node[:hostname]) || Resolv.getaddress(node[:name])
+self_hostnames = [
+  node[:fqdn],
+  node[:hostname],
+  node[:name],
+  my_ip
+]
+
+log "Seed node is : #{node[:etcd][:seed_node]}"
+log "Setting up etcd::cluster. Hosts are : #{self_hostnames.join ', '}"
+
+# if we aren't the seed then include initial cluster bootstrap
+unless self_hostnames.include? node[:etcd][:seed_node]
+  log 'This node is a slave node'
+  Etcd.slave = true
+end
+
+if Chef::Config[:solo]
+  Chef::Log.warn 'etcd requires node[:etcd][:nodes] to be set when using Chef Solo !'
+
+  # Else simply use specified nodes in :nodes array
+  cluster = node[:etcd][:nodes].dup
 else
-  msg = '`node[:etcd][:seed_node]` is required to bootstrap a cluster'
-  log msg do
-    level :error
-    not_if { node[:etcd][:seed_node] }
+  # find nodes in this env and populate the cluster nodes file with it
+  query = "recipes:#{node[:etcd][:search_cook]}"
+
+  if node[:etcd][:env_scope]
+    query << " AND chef_environment:#{node.chef_environment}"
   end
 
-  # Hostnames and/or ip addresses of current node
-  my_ip = Resolv.getaddress(node[:fqdn]) || Resolv.getaddress(node[:hostname]) || Resolv.getaddress(node[:name])
-  self_hostnames = [
-    node[:fqdn],
-    node[:hostname],
-    node[:name],
-    my_ip
-  ]
-
-  log "Seed node is : #{node[:etcd][:seed_node]}"
-  log "Setting up etcd::cluster. Hosts are : #{self_hostnames.join ', '}"
-
-  # if we aren't the seed then include initial cluster bootstrap
-  unless self_hostnames.include? node[:etcd][:seed_node]
-    log 'This node is a slave node'
-    Etcd.slave = true
-  else
-    log 'This node will be the seed node'
+  # Get a list of hosts
+  cluster = partial_search(:node, query,
+                           keys: {
+                             'node' => ['fqdn']
+                           }
+  ).map do |n|
+    # Return hostname/fqdn
+    n['node']
   end
+end
 
-  if Chef::Config[:solo]
-    Chef::Log.warn 'etcd requires node[:etcd][:nodes] to be set when using Chef Solo !'
+# Build /etc/etcd_members file
+# rubocop:disable MultilineBlockChain
+cluster_str = cluster.select do |n|
+  # Filter out current host
+  !self_hostnames.include? n
+end.map do |hostname|
+  # Get IP address
+  Resolve.getaddress hostname
+end.map do |ip|
+  # Append port
+  "#{ip}:7001"
+end.join ','  # Join in one string
+# rubocop:enable MultilineBlockChain
 
-    # Else simply use specified nodes in :nodes array
-    cluster = node[:etcd][:nodes].dup
-  else
-    # find nodes in this env and populate the cluster nodes file with it
-    query = "recipes:#{node[:etcd][:search_cook]}"
-
-    if node[:etcd][:env_scope]
-      query << " AND chef_environment:#{node.chef_environment}"
-    end
-
-    # Get a list of hosts
-    cluster = partial_search(:node, query,
-                             keys: {
-                               'node' => ['fqdn']
-                             }
-    ).map do |n|
-      # Return hostname/fqdn
-      n['node']
-    end
-  end
-
-  # Build /etc/etcd_members file
-  cluster_str = cluster.select do |n|
-    # Filter out current host
-    not self_hostnames.include? n
-  end.map do |hostname|
-    # Get IP address
-    Resolve.getaddress hostname
-  end.map do |ip|
-    # Append port
-    "#{ip}:7001"
-  end.join ','  # Join in one string
-
-  # write out members
-  file '/etc/etcd_members' do
-    content cluster_str
-  end
+# write out members
+file '/etc/etcd_members' do
+  content cluster_str
 end
 
 include_recipe 'etcd'
