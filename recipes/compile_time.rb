0dc72a8449d33a9740befd035d210fc9875824de
Jesse Nelson <spheromak@gmail.com>
2014-07-09 23:58:56 -0700

    use sugar to dry out compile_time

--- recipes/compile_time.rb
+++ recipes/compile_time.rb
@@ -6,30 +6,8 @@
 # Etcd singleton gets our node
 Etcd.node = node
 
-# HACK: to get compile time installation
-a = ark 'etcd' do
-  has_binaries ['etcd', 'etcdctl']
-  version node[:etcd][:version]
-  url Etcd.bin_url
-  checksum node[:etcd][:sha256]
-  action :install
+include_recipe 'chef-sugar'
+
+compile_time do
+  include_recipe 'etcd::default'
 end
-
-a.run_action :install
-
-directory File.dirname node[:etcd][:state_dir]
-
-t = template '/etc/init/etcd.conf' do
-  mode 0644
-  variables(args: Etcd.args)
-  notifies :restart, 'service[etcd]', :immediately
-end
-
-s = service 'etcd' do
-  provider Chef::Provider::Service::Upstart
-  supports status: true, restart: true, reload: true
-  action [:enable, :start]
-end
-
-t.run_action :create
-s.run_action :start
