6bc05f441242fcd00b3e9fec085db23771638efc
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2014-07-17 19:11:43 +0000

    Adding debian support

--- recipes/_service.rb
+++ recipes/_service.rb
@@ -3,22 +3,36 @@
 #
 #
 
-## handle rhel7
-rhel7 = false
+## handle rhel7/debian
+systemd = false
+init = false
+upstart = true
 init_provider = Chef::Provider::Service::Upstart
 
 if node[:platform_family] == 'rhel' && node[:platform_version].to_i >= 7
-  rhel7 = true
+  systemd = true
+  upstart = false
   init_provider = Chef::Provider::Service::Systemd
+end
+if node[:platform] == "debian"
+  init_provider = Chef::Provider::Service::Init::Debian
+  init=true
+  upstart=false
 end
 
 directory File.dirname node[:etcd][:state_dir]
 
-template '/etc/init/etcd.conf' do
+template "/etc/init.d/etcd" do
+  mode 0755
+  only_if { init }
+end
+
+template "/etc/default/etcd" do
   mode 0644
+  source "etcd-default.erb" 
   variables(args: Etcd.args)
   notifies :restart, 'service[etcd]' if node[:etcd][:trigger_restart]
-  not_if { rhel7 }
+  only_if { init }
 end
 
 # TODO: use this on other sytemd platforms
@@ -27,7 +41,23 @@ template '/etc/systemd/system/etcd.service' do
   variables(args: Etcd.args)
   notifies :run, 'execute[systemd_reload_units]', :immediate
   notifies :restart, 'service[etcd]' if node[:etcd][:trigger_restart]
-  only_if { rhel7 }
+  only_if { systemd }
+end
+
+template '/etc/init/etcd.conf' do
+  mode 0644
+  variables(args: Etcd.args)
+  notifies :restart, 'service[etcd]' if node[:etcd][:trigger_restart]
+  only_if { upstart }
+end
+
+# TODO: use this on other sytemd platforms
+template '/etc/systemd/system/etcd.service' do
+  mode 0644
+  variables(args: Etcd.args)
+  notifies :run, 'execute[systemd_reload_units]', :immediate
+  notifies :restart, 'service[etcd]' if node[:etcd][:trigger_restart]
+  only_if { systemd }
 end
 
 execute 'systemd_reload_units' do
