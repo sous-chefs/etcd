a6dab06fef95f017537fa86e282e37b81f662474
Sean OMeara <sean@chef.io>
2016-01-25 16:04:11 -0500

    adding marker file to test recipe

--- test/cookbooks/etcd_test/recipes/keys.rb
+++ test/cookbooks/etcd_test/recipes/keys.rb
@@ -6,7 +6,13 @@ etcd_key '/test' do
   value 'a_test_value'
 end
 
-execute 'etcdctl set /delete delete'
+execute 'etcdctl set /delete delete' do
+  not_if { ::File.exist?('/marker_etcd_key_delete') }
+end
+
+file '/marker_etcd_key_delete' do
+  action :create
+end
 
 etcd_key '/delete' do
   action :delete
