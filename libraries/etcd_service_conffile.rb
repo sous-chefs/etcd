3a1c3cd5106520df42f293d218c83bcb546acf0b
upcFrost <upcfrost@gmail.com>
2016-03-24 05:49:57 -0400

    Fixed the file and service naming to match the one used by the cookbook Packaged install provides the default service 'etcd' and the default config file name 'etcd.conf'. Added the new service file template and modified the config file template's dest name to match the pattern used in the cookbook.

--- libraries/etcd_service_conffile.rb
+++ libraries/etcd_service_conffile.rb
@@ -5,20 +5,60 @@ module EtcdCookbook
     provides :etcd_service_manager, platform: 'centos'
 
     action :create do
-      service 'etcd' do
+      # Create service template
+      template "/lib/systemd/system/#{etcd_name}.service" do
+        source 'systemd/etcd.service.conffile.erb'
+        owner 'root'
+        group 'root'
+        mode '0644'
+        variables(
+          config: new_resource,
+          etcd_name: etcd_name,
+          etcd_bin: etcd_bin
+        )
+        cookbook 'etcd'
+        notifies :run, 'execute[systemctl daemon-reload]', :immediately
+        notifies :restart, new_resource if auto_restart
+        action :create
+      end
+
+      # Register this service within Chef
+      service etcd_name do
         supports restart: true, status: true
         action [:enable]
       end
 
-      template "/etc/etcd/etcd.conf" do
+      # Create config file
+      template "/etc/etcd/#{etcd_name}.conf" do
         cookbook 'etcd'
         source 'etcd.conf.erb'
         owner 'root'
         group 'root'
         mode '0755'
-        notifies :restart, 'service[etcd]'
+        notifies :restart, new_resource
         variables config: new_resource
       end
+
+      # Avoid 'Unit file changed on disk' warning
+      execute 'systemctl daemon-reload' do
+        command '/bin/systemctl daemon-reload'
+        action :nothing
+      end
     end
+
+    action :start do
+      service etcd_name do
+        action [:start]
+      end
+    end
+    
+    action :stop do
+    end
+
+    action :restart do
+      action_stop
+      action_start
+    end
+
   end
 end
