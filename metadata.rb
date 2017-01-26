548568008ee9c3395660f98d28a6a95fc0d00297
Tim Smith <tsmith@chef.io>
2017-01-26 11:31:44 -0800

    Require the latest compat_resource
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs and configures etcd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '4.1.0'
 
-depends 'compat_resource', '>= 12.14.6'
+depends 'compat_resource', '>= 12.16.3'
 depends 'docker'
 
 supports 'amazon'
