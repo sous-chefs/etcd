40c080476ca34a4bafb829c6d8b620e9ecb4539e
Tim Smith <tsmith@chef.io>
2017-04-24 23:21:28 -0700

    Use a SPDX standard license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'etcd'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs and configures etcd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '4.1.0'
