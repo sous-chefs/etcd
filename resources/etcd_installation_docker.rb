38335bee99d6d8ef5d7d5defcf42a1933c5b5d81
Lamont Granquist <lamont@scriptkiddie.org>
2018-03-09 14:21:36 -0800

    convert to proper custom resources
    removes crazyballs inheritance that was only causing issues.
    
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

---
+++ resources/etcd_installation_docker.rb
@@ -0,0 +1,31 @@
+resource_name :etcd_installation_docker
+
+#####################
+# resource properties
+#####################
+
+property :repo, String, default: 'quay.io/coreos/etcd', desired_state: false
+property :tag, default: lazy { "v#{version}" }, desired_state: false
+property :version, default: '3.2.15'
+
+default_action :create
+
+#########
+# actions
+#########
+
+action :create do
+  docker_image 'etcd' do
+    repo new_resource.repo
+    tag new_resource.tag
+    action :pull
+  end
+end
+
+action :delete do
+  docker_image 'etcd' do
+    repo new_resource.repo
+    tag new_resource.tag
+    action :remove
+  end
+end
